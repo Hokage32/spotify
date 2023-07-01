b546feb46fc95ce5149a1a35a6333f15c7765273
Levi Raoelina <levi_r34@yahoo.com>
2023-06-30 18:06:47 -0600

    problems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
+
+import { useState,useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+ const clientId = '3e30c79fa6204a669602b306d0c3cda4'
+ const secret = 'e7ed1fce88a64051995f02a4ebf8f697'
+ 
+
+useEffect(()=> {
+  axios.post("https://accounts.spotify.com/api/token", {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      'Authorization': 'Basic ' + btoa(clientId + ':' + secret)
+    },
+    body: 'grant_type=client_credentials'
+    
+  })
+  .then((res)=> {
+    console.log(res.data.access_token)
+  })
+
+
+
+},[])
+
+
+
+
   return (
     <div className="App">
-     
+     <h1>hi</h1>
     </div>
   );
 }
